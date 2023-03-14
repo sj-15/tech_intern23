253737e3d31b1d53b3e99713eda13dd7edd5e39a
Sourav Jana <15.sj.sourav@gmail.com>
2023-03-14 21:26:07 +0530

    Initial comment

---
+++ kothabarta/android/app/src/main/kotlin/com/example/kothabarta/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kothabarta
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ tashveer/android/app/src/main/kotlin/com/example/tashveer/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.tashveer
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
