5fde8d9154ab12c71d9981cd3740159d411c802d
Sourav Jana <15.sj.sourav@gmail.com>
2023-04-15 18:10:48 +0530

    Backend

--- rashoipedia/lib/services/mysql_client.dart
+++ rashoipedia/lib/services/mysql_client.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 
 Future<List<dynamic>> getRecipesForCategory(int categoryId) async {
-  final url = Uri.parse('http://192.168.0.120:3000/recipes/$categoryId');
+  final url = Uri.parse('http://YourIPaddress:3000/recipes/$categoryId');
   final response = await http.get(url);
   if (response.statusCode == 200) {
     final data = json.decode(response.body);
